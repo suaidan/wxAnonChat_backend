e385befccc12fbf7ca98fd8e3d46644dbab867f0
苏 <3402735171@qq.com>
2017-12-01 09:07:31 +0800

    Update AnalyseMessage.js
    将处理函数handler的调用转移到socketserver.js

--- AnalyseMessage.js
+++ AnalyseMessage.js
@@ -1,6 +1,3 @@
-var handler=require("./handleHelper");
-//用来传递处理函数
-var router={};
 /***
  * 这个函数相当于路由，将不同type的数据交给不同的处理函数
  * @param message 小程序发送的消息
@@ -10,16 +7,13 @@ function analyse(message,resdata){
     var reqdata=JSON.parse(message);
     switch(reqdata.type){
         case "token":
-            router["token"]=handler.token;
-            router.token(reqdata,resdata);
+            handler.token(reqdata,resdata);
             break;
-        case "chatContent":
-            router["chatContent"]=handler.chatContent;
-            router.chatContent(reqdata,resdata);
+        case "chatContent": 
+            handler.chatContent(reqdata,resdata);
             break;
         case "indexOnload":
-            router.indexlOnload=handler.indexOnload;
-            router.indexlOnload(reqdata,resdata);
+            handler.indexlOnload(reqdata,resdata);
             break;
         default:
             break;
