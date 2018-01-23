e339c175b5842d445df757208408cad861640136
suaidan <3402735171@qq.com>
2018-01-23 23:29:08 +0800

    处理post请求

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ let util = require("util");
  */
 function start(route,portNum,handler) {
     function onRequest(request, response) {//目前只能识别简单的不附带数据的请求，未对请求类型做出验证。
+        let postContent="";
         let pathname =url.parse(request.url).pathname;
+        request.setEncoding("utf-8");
+        request.on("data",function(postChunk){
+            postContent+=postChunk;
+        })
+        request.on("end",function(){
+            route(pathname,response,handler,postContent);
+        })
         route(pathname,response,handler);
     }
     http.createServer(onRequest).listen(portNum);
